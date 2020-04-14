bacf5fbde0e8a3b4ce4aec80ee6e729a64f3411d
duanchao <1539436284@qq.com>
2020-04-14 16:08:07 +0800

    dev

--- base/base_page.py
+++ base/base_page.py
@@ -1,3 +1,4 @@
+from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 
 from utils import DriverUtil
@@ -9,13 +10,17 @@ class BasePage(object):
         self.driver = DriverUtil.get_driver()
 
     def base_find(self, location, timeout=5, poll=0.5):
-         return WebDriverWait(self.driver, timeout=timeout, poll_frequency=poll). \
+        return WebDriverWait(self.driver, timeout=timeout, poll_frequency=poll). \
             until(lambda x: x.find_element(location[0], location[1]))
 
-    def base_click(self,ele):
+    def base_click(self, ele):
         ele.click()
 
     def base_input_text(self, element, text):
         # 给某个元素输入指定内容
         element.clear()
         element.send_keys(text)
+
+    def get_toast(self, text):
+        xpath = By.XPATH, '//*contains[@text,"{}"]'.format(text)
+        return self.base_find(xpath).text
