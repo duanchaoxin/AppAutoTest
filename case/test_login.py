70c47f18867840b19db43a8310c15753c92f2ace
duanchao <1539436284@qq.com>
2020-04-14 14:52:41 +0800

    dev

--- case/test_login.py
+++ case/test_login.py
@@ -2,6 +2,9 @@
 搜索测试用例
 """
 import os, sys
+
+from page.page_factory import PageFactory
+
 sys.path.append(os.getcwd())
 import allure
 import time
@@ -10,13 +13,12 @@ import pytest
 from utils import DriverUtil, load_data
 
 
-class TestSearch(object):
+class TestLogin(object):
     """搜索测试类"""
 
     @pytest.fixture(autouse=True)
     def before_func(self):
         self.driver = DriverUtil.get_driver()  # 获取驱动对象
-        from page.page_factory import PageFactory
         self.factory = PageFactory()
 
         yield  # 结束操作
@@ -26,10 +28,8 @@ class TestSearch(object):
     @allure.MASTER_HELPER.severity(allure.MASTER_HELPER.severity_level.CRITICAL)
     @allure.MASTER_HELPER.step(title="测试步骤1")
     @pytest.mark.parametrize('text', load_data('test_search_data.yaml'))
-    def test_func(self, text):
-        allure.MASTER_HELPER.attach("搜索","1.进入搜索页 2.输入搜索内容")
-        """搜索测试方法"""
-        # 点击搜索按钮跳转搜索页面
-        self.factory.index_page().go_to_search()
-        # 输入内容搜索结果
-        self.factory.search_page().search(text)
+    def test_login(self, text):
+        allure.MASTER_HELPER.attach("登录","1.进入我的 2.点击登录页入口 3.输入登录")
+        self.factory.index_page().click_mine()
+        self.factory.mine_page().click_login()
+        self.factory.login_page().login('13157523435','lm123456')
