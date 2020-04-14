d163bfb3dd0184f4824150fd5deb25e5a218cbd7
duanchao <1539436284@qq.com>
2020-04-14 14:24:10 +0800

    master

--- page/login_page.py
+++ page/login_page.py
@@ -1,1 +1,23 @@
-class LoginPage
+from base.base_page import BasePage
+from page import user, pwd, login_btn, confirm_btn
+
+
+class LoginPage(BasePage):
+
+    def input_username(self, user_name):
+        self.base_input_text(self.base_find(user), user_name)
+
+    def input_password(self, password):
+        self.base_input_text(self.base_find(pwd), password)
+
+    def click_login(self):
+        self.base_click(self.base_find(login_btn))
+
+    def click_confirm(self):
+        self.base_click(self.base_find(confirm_btn))
+
+    def login(self, user_name, password):
+        self.input_username(user_name)
+        self.input_username(password)
+        self.click_login()
+        self.click_confirm()
--- page/mine_page.py
+++ page/mine_page.py
@@ -1,6 +1,7 @@
 from base.base_page import BasePage
+from page import login
 
 
 class MinePage(BasePage):
     def click_login(self):
-        
+        self.base_click(self.base_find(login))
